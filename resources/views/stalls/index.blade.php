332d3bd794f6578499d9b2aad328675f378fd9f4
JC <josecrizdalmacio05@gmail.com>
2025-11-17 13:53:09 +0800

    Financial dashboard clean ui

--- resources/views/stalls/index.blade.php
+++ resources/views/stalls/index.blade.php
@@ -622,15 +622,6 @@
                                             </svg>
                                             <span>{{ $stall->location }}</span>
                                         </div>
-                                        <div class="meta-rating">
-                                            <svg class="star-icon" width="16" height="16" viewBox="0 0 24 24"
-                                                fill="currentColor" stroke="currentColor" stroke-width="2">
-                                                <polygon
-                                                    points="12,2 15.09,8.26 22,9.27 17,14.14 18.18,21.02 12,17.77 5.82,21.02 7,14.14 2,9.27 8.91,8.26" />
-                                            </svg>
-                                            <span style="font-weight: 500;">4.8</span>
-                                            <span>(124)</span>
-                                        </div>
                                     </div>
 
                                     <div class="stall-info">
